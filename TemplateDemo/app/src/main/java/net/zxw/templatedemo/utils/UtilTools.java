4a9ed6a5d1567465c059f3250859e7fc79a6da29
zengxingwei <443075929@qq.com>
2018-04-08 12:34:47 +0800

    UtilTools类完善和FONT.TTF字体样式的导入

--- TemplateDemo/app/src/main/java/net/zxw/templatedemo/utils/UtilTools.java
+++ TemplateDemo/app/src/main/java/net/zxw/templatedemo/utils/UtilTools.java
@@ -1,5 +1,9 @@
 package net.zxw.templatedemo.utils;
 
+import android.content.Context;
+import android.graphics.Typeface;
+import android.widget.TextView;
+
 /**
  * @author zxw
  * @Email 18316275391@163.com
@@ -7,4 +11,9 @@ package net.zxw.templatedemo.utils;
  */
 
 public class UtilTools {
+    //设置字体
+    public static void setFont(Context mContext, TextView textview){
+        Typeface fontType = Typeface.createFromAsset(mContext.getAssets(),"fonts/FONT.TTF");
+        textview.setTypeface(fontType);
+    }
 }
